c8684f9cc243296b16c26c474566a5ecc98daf38
Kirill Antipov <kirill.antipov@finteco.io>
2023-06-24 01:52:02 +0300

    add dynamic array with tests

---
+++ School/School.UnitTests/ADS/DynArrayTests.cs
@@ -0,0 +1,98 @@
+﻿using AlgorithmsDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class DynArrayTests
+    {
+        [Fact]
+        public void Can_Create_Array()
+        {
+            var sut = new DynArray<int>();
+
+            sut.Append(1);
+
+            Assert.True(sut.count == 1);
+            Assert.True(sut.capacity == 16);
+        }
+
+        [Fact]
+        public void Array_Capacity_Enlarged_When_Current_Limit_Reached()
+        {
+            var sut = InitArray(16);
+
+            sut.Append(16);
+
+            Assert.True(sut.count == 17);
+            Assert.True(sut.capacity == 32);
+            Assert.True(sut.GetItem(0) == 0);
+            Assert.True(sut.GetItem(16) == 16);
+        }
+
+        [Fact]
+        public void Throws_Exception_On_OutOfBoundary_Call()
+        {
+            var sut = InitArray(16);
+
+            Assert.Throws<IndexOutOfRangeException>(() => sut.GetItem(17));
+            Assert.Throws<IndexOutOfRangeException>(() => sut.Insert(9999, 18));
+            Assert.Throws<IndexOutOfRangeException>(() => sut.Remove(18));
+        }
+
+        [Theory]
+        [InlineData(64, 42)]
+        [InlineData(20, 16)]
+        public void Capacity_Shrinked_When_Half_Space_NotUsed(int initialCapacity, int finalCapacity)
+        {
+            var sut = InitArray(initialCapacity);
+
+            for (int i = 0; i < initialCapacity/2; i++)
+            {
+                sut.Remove(2);
+            }
+
+            Assert.True(sut.capacity == finalCapacity);
+        }
+
+        [Fact]
+        public void Element_Removed()
+        {
+            var sut = InitArray(32);
+
+            sut.Remove(20);
+
+            Assert.True(sut.capacity == 32);
+            Assert.True(sut.GetItem(20) == 21);
+            Assert.True(sut.GetItem(19) == 19);
+        }
+
+        [Fact]
+        public void Element_Inserted()
+        {
+            var sut = InitArray(32);
+
+            sut.Insert(99, 20);
+
+            Assert.True(sut.capacity == 64);
+            Assert.True(sut.GetItem(20) == 99);
+            Assert.True(sut.GetItem(21) == 20);
+        }
+
+        private DynArray<int> InitArray(int length)
+        {
+            var array = new DynArray<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                array.Append(i);
+            }
+
+            return array;
+        }
+    }
+}
---
+++ School/School/ADS/DynArray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class DynArray<T>
+    {
+        private const int defaultCapacity = 16;
+
+        public T[] array;
+        public int count;
+        public int capacity;
+
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(defaultCapacity);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            var newArray = new T[new_capacity];
+
+            if (array != null)
+            {
+                Array.Copy(array, newArray, count);
+            }
+
+            array = newArray;
+            capacity = new_capacity;
+        }
+
+        public T GetItem(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+
+            return array[index];
+        }
+
+        public void Append(T itm)
+        {
+            if (count == array.Length)
+            {
+                MakeArray(capacity * 2);
+            }
+
+            array[count] = itm;
+            count++;
+        }
+
+        public void Insert(T itm, int index)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+
+            if (count == capacity)
+            {
+                MakeArray(capacity * 2);
+            }
+
+            Array.Copy(array, index, array, index + 1, count - index);
+            array[index] = itm;
+            count++;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+
+            Array.Copy(array, index + 1, array, index, count - index - 1);
+            count--;
+
+            if (count <= capacity / 2 && capacity > defaultCapacity)
+            {
+                int decreasedCapacity = (int)(capacity / 1.5);
+                int newCapacity = Math.Max(decreasedCapacity, defaultCapacity);
+                MakeArray(newCapacity);
+            }
+        }
+    }
+}
