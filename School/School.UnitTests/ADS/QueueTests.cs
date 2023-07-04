4cf2f8a705f8f2b94f612a07ad308e9017d90d3b
Kirill Antipov <kirill.antipov@finteco.io>
2023-07-04 13:05:12 +0300

    add queue

---
+++ School/School.UnitTests/ADS/QueueTests.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmsDataStructures;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void Queue_Works_Correctly()
+        {
+            var sut = new Queue<int>();
+
+            sut.Enqueue(1);
+            sut.Enqueue(2);
+            sut.Enqueue(3);
+
+            var result = sut.Dequeue();
+
+            Assert.True(result == 1);
+            Assert.True(sut.Size() == 2);
+        }
+    }
+}
---
+++ School/School/ADS/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Queue<T>
+    {
+        private List<T> _list;
+
+        public Queue()
+        {
+            _list = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            _list.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_list.Count == 0)
+            {
+                return default(T);
+            }
+
+            T item = _list[0];
+            _list.RemoveAt(0);
+
+            return item;
+        }
+
+        public int Size()
+        {
+            return _list.Count;
+        }
+
+    }
+}
