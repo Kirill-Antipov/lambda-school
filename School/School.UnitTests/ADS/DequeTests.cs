d9d31913621c2bc728491ac428a3f77dfe60daad
Kirill Antipov <kirill.antipov@finteco.io>
2023-07-09 13:00:30 +0300

    add deque

---
+++ School/School.UnitTests/ADS/DequeTests.cs
@@ -0,0 +1,55 @@
+﻿using AlgorithmsDataStructures;
+using System.Runtime.Remoting.Messaging;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class DequeTests
+    {
+        [Fact]
+        public void Dequeue_Works_Correctly()
+        {
+            var sut = new Deque<int>();
+
+            sut.AddFront(1);
+            sut.AddFront(0);
+            sut.AddTail(2);
+            sut.AddTail(3);
+
+            Assert.True(sut.Size() == 4);
+
+            var head = sut.RemoveFront();
+
+            Assert.Equal(0, head);
+
+            var tail = sut.RemoveTail();
+
+            Assert.Equal(3, tail);
+        }
+
+        [Fact]
+        public void Is_Palindrome()
+        {
+            var sut = new Deque<string>();
+
+            sut.AddFront("l");
+            sut.AddFront("e");
+            sut.AddFront("v");
+            sut.AddFront("e");
+            sut.AddFront("l");
+
+            var isPalindrome = true;
+
+            while (sut.Size() >=2)
+            {
+                var first = sut.RemoveFront();
+                var last = sut.RemoveTail();
+
+
+                isPalindrome = isPalindrome && first == last;
+            }
+
+            Assert.True(isPalindrome);
+        }
+    }
+}
---
+++ School/School/ADS/Deque.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Deque<T>
+    {
+        private LinkedList<T> deque;
+
+        public Deque()
+        {
+            deque = new LinkedList<T>();
+        }
+
+        public void AddFront(T item)
+        {
+            deque.AddFirst(item);
+        }
+
+        public void AddTail(T item)
+        {
+            deque.AddLast(item);
+        }
+
+        public T RemoveFront()
+        {
+            if (deque.Count == 0)
+            {
+                return default(T);
+            }
+
+            var item = deque.First.Value;
+            deque.RemoveFirst();
+
+            return item;
+        }
+
+        public T RemoveTail()
+        {
+            if (deque.Count == 0)
+            {
+                return default(T);
+            }
+
+            var item = deque.Last.Value;
+            deque.RemoveLast();
+
+            return item;
+        }
+
+        public int Size()
+        {
+            return deque.Count;
+        }
+    }
+
+}
