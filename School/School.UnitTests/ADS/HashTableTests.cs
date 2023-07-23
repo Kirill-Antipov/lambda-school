d719a00a948c922b5116b1d0a637394a4c13ec16
Kirill Antipov <kuplee.boxx@gmail.com>
2023-07-23 10:39:42 +0300

    add hashtable

---
+++ School/School.UnitTests/ADS/HashTableTests.cs
@@ -0,0 +1,37 @@
+﻿using AlgorithmsDataStructures;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class HashTableTests
+    {
+        [Fact]
+        public void HashTable_Works_Correct()
+        {
+            var sut = new HashTable(19, 3);
+
+            var emptyIndexResult = sut.Find("novalue");
+
+            Assert.True(emptyIndexResult == -1);
+
+            sut.Put("nicevalue");
+
+            var realIndex = sut.Find("nicevalue");
+
+            Assert.True(realIndex != -1);
+
+            var collisionValue1 = "collision";
+            var collisionValue2 = "conlisiol";
+
+            sut.Put(collisionValue1);
+            sut.Put(collisionValue2);
+
+            var collissionIndex1 = sut.Find(collisionValue1);
+            var collissionIndex2 = sut.Find(collisionValue2);
+
+            Assert.True(collissionIndex1 != -1);
+            Assert.True(collissionIndex2 != -1);
+            Assert.True(collissionIndex1 != collissionIndex2);
+        }
+    }
+}
---
+++ School/School/ADS/HashTable.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class HashTable
+    {
+        public int size;
+        public int step;
+        public string[] slots;
+
+        public HashTable(int sz, int stp)
+        {
+            size = sz;
+            step = stp;
+            slots = new string[size];
+            for (int i = 0; i < size; i++) slots[i] = null;
+        }
+
+        public int HashFun(string value)
+        {
+            int hash = 0;
+            foreach (char c in value)
+            {
+                hash += (int)c;
+            }
+            return hash % size;
+        }
+
+        public int SeekSlot(string value)
+        {
+            int i = 0;
+            var hash = HashFun(value);
+            int index = hash;
+
+            while (slots[index] != null)
+            {
+                i++;
+                index = StepNext(hash, i);
+
+                if (i == size)
+                {
+                    return -1;
+                }
+            }
+
+            return index;
+        }
+
+        public int Put(string value)
+        {
+            int index = SeekSlot(value);
+
+            if (index != -1)
+            {
+                slots[index] = value;
+            }
+
+            return index;
+        }
+
+        public int Find(string value)
+        {
+            int hash = HashFun(value);
+            int i = 0;
+            int index = hash;
+
+            while (slots[index] != null && slots[index] != value)
+            {
+                i++;
+                if (i == size)
+                {
+                    return -1;
+                }
+
+                index = StepNext(hash, i);
+            }
+
+            if (slots[index] != null)
+            {
+                return index;
+            }
+
+            return -1;
+        }
+
+        private int StepNext(int index, int i)
+        {
+            return (index + i * step) % size;
+        }
+    }
+
+}
