9bbfa9d20d04402209ef55d44c4fb641ea5dfae8
Kirill Antipov <kuplee.boxx@gmail.com>
2023-07-27 13:07:23 +0300

    add dictionary

---
+++ School/School.UnitTests/ADS/NativeDictionaryTests.cs
@@ -0,0 +1,91 @@
+﻿using AlgorithmsDataStructures;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class NativeDictionaryTests
+    {
+        [Fact]
+        public void Put_Adds_Value()
+        {
+            var sut = new NativeDictionary<string>(19);
+
+            sut.Put("key1", "value1");
+
+            var resultKey = sut.IsKey("key1");
+            var resultValue = sut.Get("key1");
+
+            Assert.True(resultKey);
+            Assert.True(resultValue == "value1");
+        }
+
+        [Fact]
+        public void Put_Adds_Additional_Value_When_Collision_Exists()
+        {
+            var sut = new NativeDictionary<string>(19);
+
+            sut.Put("key1", "value1");
+            sut.Put("1key", "value2");
+
+            var resultKey = sut.IsKey("1key");
+            var resultValue = sut.Get("1key");
+
+            Assert.True(resultKey);
+            Assert.True(resultValue == "value2");
+
+            resultKey = sut.IsKey("key1");
+            resultValue = sut.Get("key1");
+
+            Assert.True(resultKey);
+            Assert.True(resultValue == "value1");
+        }
+
+        [Fact]
+        public void Put_Updates_Value()
+        {
+            var sut = new NativeDictionary<string>(19);
+
+            sut.Put("key1", "value1");
+            sut.Put("key1", "value100");
+
+            var resultValue = sut.Get("key1");
+
+            Assert.True(resultValue == "value100");
+        }
+
+        [Fact]
+        public void Dictionary_Resized_When_Capacity_Large()
+        {
+            var sut = new NativeDictionary<string>(5);
+
+            sut.Put("key1", "value1");
+            sut.Put("key2", "value2");
+            sut.Put("key3", "value3");
+            sut.Put("key4", "value4");
+            sut.Put("key5", "value5");
+            sut.Put("key6", "value6");
+
+            Assert.True(sut.size == 15);
+        }
+
+        [Fact]
+        public void IsKey_False_When_NoKey()
+        {
+            var sut = new NativeDictionary<string>(19);
+
+            var resultKey = sut.IsKey("key1");
+
+            Assert.False(resultKey);
+        }
+
+        [Fact]
+        public void Get_Null_When_NoKey()
+        {
+            var sut = new NativeDictionary<string>(19);
+
+            var result = sut.Get("key1");
+
+            Assert.Null(result);
+        }
+    }
+}
---
+++ School/School/ADS/NativeDictionary.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class NativeDictionary<T>
+    {
+        public int size;
+        public string[] slots;
+        public T[] values;
+
+        private int count;
+
+        public NativeDictionary(int sz)
+        {
+            size = sz;
+            slots = new string[size];
+            values = new T[size];
+        }
+
+        public int HashFun(string key)
+        {
+            int hash = 0;
+            foreach (char c in key)
+            {
+                hash += (int)c;
+            }
+
+            return hash % size;
+        }
+
+        public bool IsKey(string key)
+        {
+            return Get(key) != null;
+        }
+
+        public void Put(string key, T value)
+        {
+            TryResize();
+            var index = SeekSlot(key);
+
+            slots[index] = key;
+            values[index] = value;
+            count++;
+        }
+
+        public T Get(string key)
+        {
+            int hash = HashFun(key);
+            int i = 0;
+            int index = hash;
+
+            while (slots[index] != null && slots[index] != key)
+            {
+                i++;
+                if (i == size)
+                {
+                    return default(T);
+                }
+
+                index = StepNext(hash, i);
+            }
+
+            if (slots[index] != null)
+            {
+                return values[index];
+            }
+
+            return default(T);
+        }
+
+        private int SeekSlot(string key)
+        {
+            int i = 0;
+            var hash = HashFun(key);
+            int index = hash;
+
+            while (slots[index] != null)
+            {
+                i++;
+                if (slots[index] == key)
+                {
+                    return index;
+                }
+
+                index = StepNext(hash, i);
+
+                if (i == size)
+                {
+                    return -1;
+                }
+            }
+
+            return index;
+        }
+
+        private int StepNext(int index, int i)
+        {
+            return (index + i * 3) % size;
+        }
+
+        public void TryResize()
+        {
+            if (count == 0)
+            {
+                return;
+            }
+
+            if (count / size < 0.78)
+            {
+                return;
+            }
+
+            size *= 3;
+            Array.Resize(ref slots, size);
+            Array.Resize(ref values, size);
+        }
+    }
+}
