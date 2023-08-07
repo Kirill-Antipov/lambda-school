9db5a8dc56957082980fc7abf60fad76ea052082
Kirill Antipov <kuplee.boxx@gmail.com>
2023-08-07 11:21:10 +0300

    add powerset

---
+++ School/School.UnitTests/ADS/PowerSetTests.cs
@@ -0,0 +1,135 @@
+﻿
+using AlgorithmsDataStructures;
+using System.Diagnostics;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class PowerSetTests
+    {
+        [Fact]
+        public void PowerSet_Works()
+        {
+            var sut = new PowerSet<string>();
+
+            sut.Put("abra");
+
+            Assert.True(sut.Get("abra"));
+
+            sut.Put("abra");
+
+            Assert.True(sut.Size() == 1);
+
+            sut.Remove("abra");
+
+            Assert.True(sut.Size() == 0);
+
+            Assert.False(sut.Get("abra"));
+        }
+
+        [Fact]
+        public void Union_Works_Correct()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(5, 5);
+
+            var sut = set1.Union(set2);
+
+            Assert.True(sut.Size() == 10);
+        }
+
+        [Fact]
+        public void Intersection_Works_Correct()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(5, 1);
+
+            var sut = set1.Intersection(set2);
+
+            Assert.True(sut.Size() == 4);
+            Assert.True(sut.Get("1"));
+            Assert.False(sut.Get("0"));
+        }
+
+        [Fact]
+        public void Intersection_Works_Correct_When_Empty_Set_Expetced()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(5, 5);
+
+            var sut = set1.Intersection(set2);
+
+            Assert.True(sut.Size() == 0);
+        }
+
+        [Fact]
+        public void Difference_Works_Correct_When_Empty_Set_Expetced()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(5);
+
+            var sut = set1.Difference(set2);
+
+            Assert.True(sut.Size() == 0);
+        }
+
+        [Fact]
+        public void Difference_Works_Correct()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(5, 1);
+
+            var sut = set1.Difference(set2);
+
+            Assert.True(sut.Get("0"));
+        }
+
+        [Fact]
+        public void IsSubset_Returns_True_When_Contains_All_Elemetns()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(3);
+
+            var result = set2.IsSubset(set1);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsSubset_Returns_False_When_Mismatch_Exists()
+        {
+            var set1 = GenerateDataSet(5);
+            var set2 = GenerateDataSet(5, 1);
+
+            var result = set2.IsSubset(set1);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Intersection_Works_Fast()
+        {
+            var set1 = GenerateDataSet(8000);
+            var set2 = GenerateDataSet(8000);
+
+            var sp = new Stopwatch();
+            sp.Start();
+            set1.Intersection(set2);
+            sp.Stop();
+
+            Assert.True(sp.ElapsedMilliseconds < 3 * 1000);
+        }
+
+        private PowerSet<string> GenerateDataSet(int size, int seed = 0)
+        {
+            var set = new PowerSet<string>();
+            for (var i = 0; i < size; i++)
+            {
+                var element = seed + i;
+                set.Put(element.ToString());
+            }
+
+            return set;
+        }
+    }
+}
---
+++ School/School/ADS/PowerSet.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class PowerSet<T>
+    {
+        public int size;
+        public int step;
+        public T[] slots;
+        public List<T> values;
+
+        public PowerSet()
+        {
+            size = 20000;
+            step = 3;
+            slots = new T[size];
+            values = new List<T>(size);
+        }
+
+        public int Size()
+        {
+            return values.Count;
+        }
+
+        public void Put(T value)
+        {
+            if (Get(value))
+            {
+                return;
+            }
+
+            int index = SeekSlot(value);
+
+            if (index != -1)
+            {
+                slots[index] = value;
+                values.Add(value);
+            }
+        }
+
+        public bool Get(T value)
+        {
+            return Find(value) != -1;
+        }
+
+        public bool Remove(T value)
+        {
+            var index = Find(value);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            slots[index] = default(T);
+            values.Remove(value);
+
+            return true;
+        }
+
+        public PowerSet<T> Intersection(PowerSet<T> set2)
+        {
+            var result = new PowerSet<T>();
+
+            foreach (T item in values)
+            {
+                if (set2.Get(item))
+                {
+                    result.Put(item);
+                }
+            }
+
+            return result;
+        }
+
+        public PowerSet<T> Union(PowerSet<T> set2)
+        {
+            var result = new PowerSet<T>();
+
+            foreach (T item in values)
+            {
+                result.Put(item);
+            }
+
+            foreach (T item in set2.values)
+            {
+                result.Put(item);
+            }
+
+            return result;
+        }
+
+        public PowerSet<T> Difference(PowerSet<T> set2)
+        {
+            var result = new PowerSet<T>();
+
+            foreach (T item in values)
+            {
+                if (!set2.Get(item))
+                {
+                    result.Put(item);
+                }
+            }
+
+            return result;
+        }
+
+        public bool IsSubset(PowerSet<T> set2)
+        {
+            foreach (var item in values)
+            {
+                if (!set2.Get(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int SeekSlot(T value)
+        {
+            var hash = GetHashCode(value);
+            int index = hash;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (slots[index] == null)
+                {
+                    return index;
+                }
+
+                index = StepNext(hash, i);
+            }
+
+            return -1;
+        }
+
+        public int Find(T value)
+        {
+            int hash = GetHashCode(value);
+            int index = hash;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (slots[index] != null && slots[index].Equals(value))
+                {
+                    return index;
+                }
+
+                index = StepNext(hash, i);
+            }
+
+            return -1;
+        }
+
+        private int GetHashCode(T value)
+        {
+            return Math.Abs(value.GetHashCode()) % size;
+        }
+
+        private int StepNext(int index, int i)
+        {
+            return (index + i * step) % size;
+        }
+    }
+}
