3c1d9a7f8430712e2a23adf51e69587bfddb4c04
Kirill Antipov <kuplee.boxx@gmail.com>
2023-08-14 23:55:08 +0300

    add bloomfilter

---
+++ School/School.UnitTests/ADS/BloomFilterTests.cs
@@ -0,0 +1,28 @@
+﻿using AlgorithmsDataStructures;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class BloomFilterTests
+    {
+        [Fact]
+        public void BloomFilter_Sustainable()
+        {
+            var sut = new BloomFilter(32);
+
+            sut.Add("0123456789");
+
+            var result = sut.IsValue("0123456789");
+
+            Assert.True(result);
+
+            result = sut.IsValue("1234567890");
+
+            Assert.False(result);
+
+            var falsePosotoveResult = sut.IsValue("2345678901");
+
+            Assert.True(falsePosotoveResult);
+        }
+    }
+}
---
+++ School/School/ADS/BloomFilter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace AlgorithmsDataStructures
+{
+    public class BloomFilter
+    {
+        public int filter_len;
+        public int filter;
+
+        public BloomFilter(int f_len)
+        {
+            filter_len = f_len;
+            filter = 0;
+        }
+
+        public int Hash1(string str1)
+        {
+            return Hash(str1, 17);
+        }
+
+        public int Hash2(string str1)
+        {
+            return Hash(str1, 223);
+        }
+
+        public void Add(string str1)
+        {
+            var mask = GetMask(str1);
+
+            filter |= mask;
+        }
+
+        public bool IsValue(string str1)
+        {
+            var mask = GetMask(str1);
+            return (filter & mask) != 0;
+        }
+
+        private int GetMask(string str1)
+        {
+            var hashCollection = new int[] { Hash1(str1), Hash2(str1) };
+            var mask = 0;
+
+            foreach (var hash in hashCollection)
+            {
+                var shift = hash == 0 ? hash : hash - 1;
+                mask |= 1 << shift;
+            }
+
+            return mask;
+        }
+
+        private int Hash(string value, int rnd)
+        {
+            int hash = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                hash *= rnd;
+                hash += (int)value[i];
+                hash %= filter_len;
+            }
+
+            return hash;
+        }
+    }
+}
