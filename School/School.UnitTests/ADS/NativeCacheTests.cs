f8ea9610a2ce344a2b94199c53603dd0c2bb3cc8
Kirill Antipov <kuplee.boxx@gmail.com>
2023-08-19 21:51:20 +0300

    add simple cache implementation

---
+++ School/School.UnitTests/ADS/NativeCacheTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+
+namespace School.UnitTests.ADS
+{
+    public class NativeCacheTests
+    {
+        [Fact]
+        public void Element_Stored_In_Cache()
+        {
+            var sut = new NativeCache<string>(3);
+
+            sut.Put("key1", "nice_value");
+
+            Assert.True(sut.Get("key1") == "nice_value");
+        }
+
+        [Fact]
+        public void Slot_Freed_For_New_Element_When_Capacity_Reached()
+        {
+            var sut = new NativeCache<string>(5);
+
+            sut.Put("key1", "nice_value");
+            sut.Put("ke1y", "good_value");
+            sut.Put("k1ey", "sunny_value");
+            sut.Put("1key", "stormy_value");
+            sut.Put("1kye", "funky_value");
+
+            sut.Get("ke1y");
+            sut.Get("key1");
+
+            sut.Put("key2", "spicy_value");
+
+            Assert.Null(sut.Get("k1ey"));
+            Assert.True(sut.Get("key2") == "spicy_value");
+        }
+
+        [Fact]
+        public void Key_Hits_Correct()
+        {
+            var sut = new NativeCache<string>(5);
+
+            sut.Put("key0", "nice_value");
+            sut.Put("key1", "greedy_value");
+            sut.Put("key2", "piggy_value");
+
+            var key0Index = Array.FindIndex(sut.slots, v => v == "key0");
+            var key1Index = Array.FindIndex(sut.slots, v => v == "key1");
+            var key2Index = Array.FindIndex(sut.slots, v => v == "key2");
+
+            sut.Get("key1");
+            sut.Get("key2");
+            sut.Get("key2");
+
+            Assert.True(sut.hits[key0Index] == 0);
+            Assert.True(sut.hits[key1Index] == 1);
+            Assert.True(sut.hits[key2Index] == 2);
+        }
+    }
+}
---
+++ School/School/ADS/NativeCache.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+public class NativeCache<T>
+{
+    private int count;
+
+    public int size;
+    public string[] slots;
+    public T[] values;
+    public int[] hits;
+
+    public NativeCache(int size)
+    {
+        this.size = size;
+        slots = new string[size];
+        values = new T[size];
+        hits = new int[size];
+    }
+
+    public void Put(string key, T value)
+    {
+        var index = count < size ? SeekSlot(key) : FreeSlot();
+
+        slots[index] = key;
+        values[index] = value;
+        count++;
+    }
+
+    public T Get(string key)
+    {
+        int hash = HashFun(key);
+        int index = hash;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (slots[index] != null && slots[index].Equals(key))
+            {
+                hits[index]++;
+                return values[index];
+            }
+
+            index = StepNext(hash, i);
+        }
+
+        return default(T);
+    }
+
+    private int FreeSlot()
+    {
+        var freeIndex = Array.IndexOf(hits, hits.Min());
+
+        slots[freeIndex] = null;
+        values[freeIndex] = default(T);
+        hits[freeIndex] = 0;
+        count--;
+
+        return freeIndex;
+    }
+
+    private int SeekSlot(string key)
+    {
+        var hash = HashFun(key);
+        int index = hash;
+
+        for (int i = 1; i <= size; i++)
+        {
+            if (slots[index] == null)
+            {
+                return index;
+            }
+
+            index = StepNext(hash, i);
+        }
+
+        return -1;
+    }
+
+    private int StepNext(int index, int i)
+    {
+        return (index + i) % size;
+    }
+
+    private int HashFun(string key)
+    {
+        int hash = 0;
+        foreach (char c in key)
+        {
+            hash += (int)c;
+        }
+
+        return hash % size;
+    }
+}
