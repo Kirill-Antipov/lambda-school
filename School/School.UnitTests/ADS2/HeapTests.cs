d19f7d86ddc8057f35c89cf0c0ea8c84161ecbac
Kirill Antipov <kuplee.boxx@gmail.com>
2023-11-26 22:14:44 +0300

    ads2: heap

---
+++ School/School.UnitTests/ADS2/HeapTests.cs
@@ -0,0 +1,61 @@
+﻿using AlgorithmsDataStructures2;
+using Xunit;
+
+namespace School.UnitTests.ADS2
+{
+    public class HeapTests
+    {
+        [Fact]
+        public void Heap_Created()
+        {
+            var inputArray = new int[] {1, 5, 9, 3, 2, 4 };
+            var heap = new Heap();
+
+            heap.MakeHeap(inputArray, 4);
+
+            Assert.True(heap.HeapArray.Length == 31);
+            Assert.True(heap.CurrentSize == 6);
+            Assert.True(heap.HeapArray[0] == 9);
+            Assert.True(heap.HeapArray[5] == 1);
+        }
+
+        [Fact]
+        public void Max_Element_Retrieved_With_Heap_Rebuild()
+        {
+            var inputArray = new int[] { 1, 5, 9, 3, 2, 4 };
+            var heap = new Heap();
+
+            heap.MakeHeap(inputArray, 4);
+
+            var result = heap.GetMax();
+
+            Assert.Equal(9, result);
+            Assert.True(heap.CurrentSize == 5);
+            Assert.True(heap.HeapArray[0] == 5);
+        }
+
+        [Fact]
+        public void Max_Element_Retrieved_When_Heap_Empty()
+        {
+            var heap = new Heap();
+
+            var result = heap.GetMax();
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void Key_Added_To_Heap()
+        {
+            var inputArray = new int[] { 7, 5, 9, 3, 2, 4 };
+            var heap = new Heap();
+
+            heap.MakeHeap(inputArray, 4);
+
+            heap.Add(10);
+
+            Assert.Equal(7, heap.CurrentSize);
+            Assert.Equal(10, heap.HeapArray[0]);
+        }
+    }
+}
---
+++ School/School/ADS2/Heap.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Heap
+    {
+        public int[] HeapArray;
+
+        public int CurrentSize;
+
+        public Heap() { HeapArray = null; }
+
+        public void MakeHeap(int[] a, int depth)
+        {
+            int maxElements = (int)Math.Pow(2, depth + 1) - 1;
+
+            if (a.Length > maxElements)
+            {
+                throw new ArgumentException($"Specified depth is incorrect. Depth: {depth}, initial array size: {a.Length}");
+            }
+
+            HeapArray = new int[maxElements];
+            CurrentSize = a.Length;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                HeapArray[i] = a[i];
+            }
+
+            for (int i = (HeapArray.Length - 2) / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public int GetMax()
+        {
+            if (CurrentSize == 0)
+            {
+                return -1;
+            }
+
+            int max = HeapArray[0];
+            HeapArray[0] = HeapArray[CurrentSize - 1];
+            CurrentSize--;
+            Heapify(0);
+
+            return max;
+        }
+
+        public bool Add(int key)
+        {
+            if (CurrentSize == HeapArray.Length)
+            {
+                return false;
+            }
+
+            HeapArray[CurrentSize] = key;
+            CurrentSize++;
+
+            SiftUp(CurrentSize - 1);
+
+            return true;
+        }
+
+        private void Heapify(int index)
+        {
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+            int largest = index;
+
+            if (leftChild < CurrentSize && HeapArray[leftChild] > HeapArray[largest])
+            {
+                largest = leftChild;
+            }
+
+            if (rightChild < CurrentSize && HeapArray[rightChild] > HeapArray[largest])
+            {
+                largest = rightChild;
+            }
+
+            if (largest != index)
+            {
+                Swap(index, largest);
+                Heapify(largest);
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            int temp = HeapArray[i];
+            HeapArray[i] = HeapArray[j];
+            HeapArray[j] = temp;
+        }
+
+        private void SiftUp(int index)
+        {
+            if (index <= 0)
+            {
+                return;
+            }
+
+            int parentIndex = (index - 1) / 2;
+
+            if (HeapArray[index] > HeapArray[parentIndex])
+            {
+                Swap(index, parentIndex);
+                SiftUp(parentIndex);
+            }
+        }
+    }
+}
